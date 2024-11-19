07280ce689f06bc7b7571d995599e933155052f4
GodofLiu <l765054414@gmail.com>
2024-11-19 18:04:25 +0800

    1

--- app.py
+++ app.py
@@ -9,6 +9,29 @@ def hello_world_fn(username: str) -> tuple[str, str]:
         return f"opus! some exception {e}\n{traceback.format_exc()}", "FAILED"
 
 
+def parser_fn(html):
+    result = []
+    html_len = len(html)
+    i = 0
+    while i < html_len:
+        if html[i:i+3] == "<p>":
+            i+=3
+            text = ""
+            while i < html_len and html[i:i+4] !="</p>":
+                if html[i]=="<":
+                    while i<html_len and html[i] != ">":
+                        i+=1
+                    i+=1
+                else:
+                    text +=html[i]
+                    i += 1
+            result.append(text.strip())
+            i += 4
+        else:
+            i += 1
+    return "\n".join(result)
+
+
 def main() -> None:
     with gr.Blocks(title="DeepLang Data test project") as demo:
         with gr.Tab("hello world 0"):
@@ -33,6 +56,18 @@ def main() -> None:
                 fn=hello_world_fn,
                 inputs=raw_input,
                 outputs=[pack_output, status_output],
+            )
+
+        with gr.Tab("htme parser"):
+            raw_input = gr.Textbox(label="")
+            pack_output = gr.Textbox(label="输出")
+
+
+            btn = gr.Button("开始转换")
+            btn.click(
+                fn=parser_fn,
+                inputs=raw_input,
+                outputs=[pack_output],
             )
 
     demo.queue(default_concurrency_limit=100).launch(
